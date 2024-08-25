330aeb68f8f8789242ebfe29ee0350d580e72319
Dennis Salzmann <dennis.salzmann@dfki.de>
2024-08-25 19:52:15 +0200

    fix: serve

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,21 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
 
 
 
-@NgModule({
-  declarations: [
-    HeaderComponent
-  ],
-  imports: [
-    CommonModule,
-    RouterModule,
-    NgbModule,
-    HttpClientModule
-  ],
-  exports: [ HeaderComponent ]
-})
+@NgModule({ declarations: [
+        HeaderComponent
+    ],
+    exports: [HeaderComponent], imports: [CommonModule,
+        RouterModule,
+        NgbModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class CoreModule { }
